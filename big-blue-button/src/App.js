a115cb49f56c2756c9eef646aa49cffa57345a76
CosmicJougan <mbielen99@gmail.com>
2022-09-25 21:50:29 +0200

    navbar wooo baby

--- big-blue-button/src/App.js
+++ big-blue-button/src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import Registration from "./components/registration/registration";
 import Login from "./components/login/login";
 import Buttons from "./components/buttons/buttons";
+import ResponsiveAppBar from "./components/navBar/navBar";
 
 function App() {
   return (
-    <div className="App-header">
-      <Buttons />
-    </div>
+      <div className="App-header">
+      <ResponsiveAppBar/>
+        <Buttons />
+      </div>
+
   );
 }
 
